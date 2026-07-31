8edead73d5819125ab51d123af5e3079716ff8d0
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2026-08-01 01:13:31 +0530

    first commit

---
+++ config/create_logs.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import { LOGS_FOLDER,PROJECT_ROOT } from '../fileConfig.js';
+class Logs {
+    constructor(mode = 'health',folder_name,file_name){
+        this.mode = mode;
+        this.date = new Date();
+        this.log_folder_name = folder_name || `Date_${this.date.getFullYear()}/${this.date.getMonth() + 1}/${this.date.getDate()}`;
+        this.log_file_name = file_name || `${this.date.getHours()}-${this.date.getMinutes()}-${this.date.getSeconds()}.log`;
+    }
+    init(){
+        if(!fs.existsSync(LOGS_FOLDER)){
+            fs.mkdir(LOGS_FOLDER, { recursive: true }, (err) => {
+                if(err) throw err;
+            })
+        }
+        if(!fs.existsSync(`${LOGS_FOLDER}/${this.mode}/${this.log_folder_name}`)){
+            fs.mkdir(`${LOGS_FOLDER}/${this.mode}/${this.log_folder_name}`, { recursive: true }, (err) => {
+                if(err) throw err;
+            })
+        }
+    }
+    getLogs(mode = this.mode){
+
+    }
+}
+
---
+++ fileConfig.js
@@ -0,0 +1,7 @@
+import fs from 'fs';
+
+const PROJECT_ROOT = process.cwd();;
+const LOGS_FOLDER = `${PROJECT_ROOT}/logs`;
+
+
+export { PROJECT_ROOT, LOGS_FOLDER };
