246e7b9489b8ef2c52cb8ce4293cc4e86494e649
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2026-08-01 14:25:37 +0530

    health api

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ import { configDotenv } from "dotenv";
 configDotenv();
 import {app} from "./app.js";
 import { logs } from "./utils/logger.js";
-
+app.get("/api/v1/health",(req,resp)=>{
+    resp.status(200).send({
+        data:{},
+        message:"Backend health checked!"
+    })
+})
 app.listen(process.env.BACKEND_PORT,(err)=>{
     if(err){
         logs.error("Cant connect")
     }else{
-        logs.info("connected")
+        logs.info(`Server connected at port ${process.env.BACKEND_PORT}`)
     }
 })
---
+++ utils/mailer.js
@@ -0,0 +1,6 @@
+class Mailer {
+    constructor(user,app_pass){
+        this.smtp_user = user;
+        this.app_pass = app_pass;
+    }
+}
