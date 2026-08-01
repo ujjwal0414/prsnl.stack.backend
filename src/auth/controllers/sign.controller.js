fcf9092e3f9b9153919e21bcc208776d5f1dede7
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2026-08-01 23:48:26 +0530

    zod user done

--- src/auth/controllers/sign.controller.js
+++ src/auth/controllers/sign.controller.js
@@ -1,6 +1,31 @@
 import asyncHandler from "express-async-handler";
 import { userModel } from "../schemas/user.schema.js";
+import { userZodSchema } from "../schemas/user.zod.js";
+import { logs } from "../../../utils/logger.js";
 const signUp = asyncHandler(async(req,resp)=>{
     const {userEmail,password} = req.body;
-
+    const validData = await userZodSchema.safeParseAsync({
+        userEmail:userEmail,
+        password:password
+    });
+    if(!validData.success){
+        resp.status(403).send({
+            message:"Validation failed",
+            data:validData?.error?.issues
+        })
+    }
+    const checkUserExistence = await userModel.findOne({userEmail})
+    if(checkUserExistence){
+        resp.status(203).send({
+            message:"User already present",
+            data:checkUserExistence
+        })
+        return
+    }
+    const createUser = await userModel.insertOne({userEmail,password});
+    resp.status(201).send({
+        message:"User created successfully",
+        data:createUser
+    })
 })
+export {signUp}
--- src/auth/routes/auth.route.js
+++ src/auth/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { login } from "../controllers/auth.controller.js";
+import { signUp } from "../controllers/sign.controller.js";
 const authRouter = express.Router();
 authRouter.post("/login",login)
+authRouter.post("/signUp",signUp)
 export {authRouter}
