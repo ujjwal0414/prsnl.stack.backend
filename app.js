d64d14bc5df43da1bc18ee950f2707c94662897e
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2026-08-01 12:44:10 +0530

    auth route creation

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 import express from "express"
+import cookieParser from "cookie-parser";
+import { authRouter } from "./src/auth/routes/auth.route.js";
 const app = express();
+app.use(cors({
+    origin: '*',
+}))
 app.use(express.json());
-app.get("/",(req,resp)=>{
-    resp.send("running")
-})
+app.use(express.urlencoded({limit:"16kb"}))
+app.use(cookieParser());
+
+app.use("/api/v1/auth",authRouter);
 export {app}
