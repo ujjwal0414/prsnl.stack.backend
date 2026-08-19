14620b6115a76108da057e979e7138489a773528
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2026-08-19 16:45:42 +0530

    createService function creation

---
+++ src/vendor/controllers/createService.controller.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import asyncHandler from "express-async-handler"
+import { logs } from "../../../utils/logger.js";
+import { sendResponse } from "express-res-handler";
+import { sendResponse } from "express-res-handler";
+const createServide = asyncHandler(async(req,resp)=>{
+    const serviceData = req.body;
+    sendResponse(resp,200,true,serviceData,"Service created")
+})
+export {createServide}
--- src/vendor/routes/vendor.route.js
+++ src/vendor/routes/vendor.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { isUserAuthenticated } from "../../middlewares/userAuthMiddleware.js";
 import { getVendor } from "../controllers/getVendor.controller.js";
+import { createServide } from "../controllers/createService.controller.js";
 const vendorRouter = Router();
 vendorRouter.use(isUserAuthenticated)
 vendorRouter.get("/getVendorAuth",getVendor)
-
+vendorRouter.post("/createService",createServide)
   
 export {vendorRouter}     
